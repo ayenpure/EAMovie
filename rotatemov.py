2eb3fc723e6969ab6e028870fb800bd2157a7cb1
Abhishek Yenpure <abhishek.yenpure@hotmail.com>
2023-12-12 11:31:26 -0800

    Adding updated script

--- rotatemov.py
+++ rotatemov.py
@@ -15,53 +15,63 @@ LoadPlugin("/home/local/KHQ/abhi.yenpure/repositories/eam/scripts/eam_reader.py"
 LoadPlugin("/home/local/KHQ/abhi.yenpure/repositories/eam/scripts/eam_filters.py", ns=globals())
 LoadPlugin("/home/local/KHQ/abhi.yenpure/repositories/eam/scripts/eam_projection.py", ns=globals())
 
-LoadState("/home/local/KHQ/abhi.yenpure/repositories/eam/forMovie/states/state_slice_lat_lev_rotate_layout.pvsm")
+LoadState("/home/local/KHQ/abhi.yenpure/repositories/eam/forMovie/states/state_slice_lat_lev_rotate_layout_radar.pvsm")
 
 # find source
-v2_F2010_nc00_inst_macmic01eamh02010122500000nc = FindSource('v2_F2010_nc00_inst_macmic01.eam.h0.2010-12-25-00000.nc')
-print(v2_F2010_nc00_inst_macmic01eamh02010122500000nc)
+data = FindSource('Data')
+print(data)
 # set active source
-SetActiveSource(v2_F2010_nc00_inst_macmic01eamh02010122500000nc)
+SetActiveSource(data)
 # Properties modified on v2_F2010_nc00_inst_macmic01eamh02010122500000nc
-v2_F2010_nc00_inst_macmic01eamh02010122500000nc.a3DMiddleLayerVariables = ['cnd01_ALST_ACTDIAG01', 'cnd01_AST_ACTDIAG01', 'cnd01_CDMC_ACTDIAG01', 'cnd01_CDNC_ACTDIAG01', 'cnd01_RAL_ACTDIAG01', 'cnd01_REL_ACTDIAG01']
+data.a2DVariables = ['CLDLOW', 'CLDTOT']
+data.a3DMiddleLayerVariables = ['cnd01_ALST_ACTDIAG01', 'cnd01_AST_ACTDIAG01', 'cnd01_CDMC_ACTDIAG01', 'cnd01_CDNC_ACTDIAG01', 'cnd01_RAL_ACTDIAG01', 'cnd01_REL_ACTDIAG01']
 
-# get active view
-renderView1 = GetActiveViewOrCreate('RenderView')
+# find view
+renderView1 = FindViewOrCreate('RenderView1', viewtype='RenderView')
+renderView2 = FindViewOrCreate('RenderView2', viewtype='RenderView')
 
 # get layout
 layout1 = GetLayout()
 
 # layout/tab size in pixels
-layout1.SetSize(1988, 1144)
+layout1.SetSize(1970, 1144)
 
 sin = math.sin
 r   = math.radians
 cos = math.cos
 
 # find source
-slice1 = FindSource('Slice1')
+a2DDataCurtain  = FindSource('2DDataCurtain')
+a2DRadarLine    = FindSource('2DRadarLine')
 # set active source
-SetActiveSource(slice1)
+#SetActiveSource(slice1)
+
+# current camera placement for renderView2
+renderView2.CameraPosition = [0.0, 0.0, 9275.129857372207]
+renderView2.CameraFocalPoint = [0.0, 0.0, 0.510986328125]
+renderView2.CameraParallelScale = 6378.5863535127
 
 for i in range(1, 361): 
     # Properties modified on slice1.SliceType
-    slice1.SliceType.Origin = [i, 78.25, 799.2482197458811]
+    a2DDataCurtain.SliceType.Origin = [i, 78.25, 799.2482197458811]
+    a2DRadarLine.longitude = i
 
     a = i
+    # Clockwise rotation
     rZmatrix = np.array([[cos(r(a)), sin(r(a)), 0, 0],
                         [-sin(r(a)), cos(r(a)), 0, 0],
                         [0, 0, 1, 0],
                         [0, 0, 0, 1]])
-    ocam    = np.array([58.817138671875, 2167.9455323715842, 2112.3284912109375, 1.0])
+    ocam    = np.array([398.1654383050872, -3174.0890539452366, 2183.232066879151, 1.0])
     cam     = ocam.dot(rZmatrix)
-    ofoc    = np.array([58.817138671875, 0.0, 2112.3284912109375, 1.0])
+    ofoc    = np.array([398.1654383050872, 0.0, 2183.232066879151, 1.0])
     foc     = ofoc.dot(rZmatrix)
     '''
     # current camera placement for renderView1
-    renderView1.CameraPosition = [58.817138671875, 2167.9455323715842, 2112.3284912109375]
-    renderView1.CameraFocalPoint = [58.817138671875, 0.0, 2112.3284912109375]
+    renderView1.InteractionMode = '2D'
+    renderView1.CameraPosition = [398.1654383050872, -3174.0890539452366, 2183.232066879151]
+    renderView1.CameraFocalPoint = [398.1654383050872, 0.0, 2183.232066879151]
     renderView1.CameraViewUp = [0.0, 0.0, 1.0]
-    renderView1.CameraParallelScale = 1202.7796693600574
     '''
     renderView1.CameraPosition = [cam[0], cam[1], cam[2]]
     renderView1.CameraFocalPoint = [foc[0], foc[1], foc[2]]
@@ -70,52 +80,11 @@ for i in range(1, 361):
 
     # update the view to ensure updated data information
     renderView1.Update()
+    renderView2.Update()
 
     x = ('%04d' %i)
     filename = 'rotateZ' + x + '.png'
+    print("Saving ", i)
     # save screenshot
     SaveScreenshot(filename, layout1, 16, SaveAllViews=1, ImageResolution=[1988, 1144])
-'''
-# get layout
-layout1 = GetLayout()
 
-# layout/tab size in pixels
-layout1.SetSize(1979, 1136)
-
-# find view
-renderView1 = FindViewOrCreate('RenderView1', viewtype='RenderView')
-
-# find view
-renderView2 = FindViewOrCreate('RenderView2', viewtype='RenderView')
-
-# find view
-renderView4 = FindViewOrCreate('RenderView4', viewtype='RenderView')
-
-# current camera placement for renderView1
-renderView1.CameraPosition = [175.25241088867188, 3023.123900962093, 2416.484130859375]
-renderView1.CameraFocalPoint = [175.25241088867188, 0.0, 2416.484130859375]
-renderView1.CameraViewUp = [0.0, 0.0, 1.0]
-renderView1.CameraParallelScale = 1386.1438050807299
-
-# current camera placement for renderView2
-renderView2.CameraPosition = [175.25241088867188, 3023.123900962093, 2416.484130859375]
-renderView2.CameraFocalPoint = [175.25241088867188, 0.0, 2416.484130859375]
-renderView2.CameraViewUp = [0.0, 0.0, 1.0]
-renderView2.CameraParallelScale = 1386.1438050807299
-
-# current camera placement for renderView3
-renderView3.CameraPosition = [175.25241088867188, 3023.123900962093, 2416.484130859375]
-renderView3.CameraFocalPoint = [175.25241088867188, 0.0, 2416.484130859375]
-renderView3.CameraViewUp = [0.0, 0.0, 1.0]
-renderView3.CameraParallelScale = 1386.1438050807299
-
-# current camera placement for renderView4
-renderView4.CameraPosition = [175.25241088867188, 3023.123900962093, 2416.484130859375]
-renderView4.CameraFocalPoint = [175.25241088867188, 0.0, 2416.484130859375]
-renderView4.CameraViewUp = [0.0, 0.0, 1.0]
-renderView4.CameraParallelScale = 1386.1438050807299
-
-# save screenshot
-SaveScreenshot('/home/local/KHQ/abhi.yenpure/repositories/eam/forMovie/screenshot.png', layout1, 16, SaveAllViews=1,
-    ImageResolution=[1979, 1136])
-'''
